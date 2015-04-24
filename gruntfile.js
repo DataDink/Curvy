96aaefaadc746b6e742cc9d1f323ed663b98dc04
Mark Greenwald <markerdink@gmail.com>
2015-04-24 13:52:35 -0700

    Docs site setup
    It just feels like there should be docs

--- gruntfile.js
+++ gruntfile.js
@@ -1,43 +1,34 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
+		less: {
+			index: {
+				compress: true,
+				files: {
+					'styles/themes/default.css' : 'styles/themes/default.less'
+				}
+			}
+		},
 		concat: {
 			options: { separator: ';\r\n\r\n' },
 			curvy: {
 				src: [
-					'src/core.js',
-					'src/services/utilities.js',
-					'src/services/broadcast.js',
-					'src/services/bindings.js',
-					'src/services/http.js',
-					'src/services/html.js',
-					'src/services/route.js',
-					
-					'src/bindings/data-routed.js',
-					'src/bindings/data-template.js',
-					'src/bindings/data-view.js',
-					'src/bindings/view-model.js',
-					'src/bindings/data-format.js',
-					'src/bindings/data-bind.js',
-					'src/bindings/data-html.js',
-					'src/bindings/data-src.js',
-					'src/bindings/data-href.js',
-					'src/bindings/data-title.js',
-					'src/bindings/data-show.js',
-					'src/bindings/data-hide.js',
-					'src/bindings/data-class.js',
-					'src/bindings/data-submit.js',
-					'src/bindings/data-click.js'
+					'builds/curvy.js',
+					'scripts/application.js',
+					'scripts/viewmodels/*.js'
 				],
-				dest: 'builds/curvy.js'
+				dest: 'scripts/views/index.js'
 			}
 		},
 		uglify: {
-			index: { src: 'builds/curvy.js', dest: 'builds/curvy.min.js' }
+			index: { src: 'scripts/views/index.js', dest: 'scripts/views/index.min.js' }
 		}
 	});
 	
+	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('build-all', ['concat', 'uglify']);
+	grunt.registerTask('build-all', ['less', 'concat', 'uglify']);
+	grunt.registerTask('build-styles', ['less']);
+	grunt.registerTask('build-scripts', ['concat', 'uglify']);
 };
 	
