27432fffbaa16a7dcdcdf3c1c174fba6101ba352
Mark Greenwald <markerdink@gmail.com>
2015-05-04 15:46:11 -0700

    Addt tests

--- src/services/html.js
+++ src/services/html.js
@@ -7,14 +7,14 @@
 
 	Application.extend.register.perApp('html', ['utilities', 'application', function(utils, app) {
 		var service = this;
-		
+
 		// ENCODE: encodes HTML so that it can be displayed
 		service.encode = function(str) {
 			if (str.indexOf('"') < 0 && str.indexOf("'") < 0 && str.indexOf('<') < 0 && str.indexOf('>') < 0) { return str; }
 			return (str || '').toString().replace(/&/g, '&amp;').replace(/"/g, '&quot;')
 				.replace(/'/g, '&#39;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 		};
-		
+
 		// PARSE: parses html into DOM nodes
 		service.parse = function(str) {
 			var nodes = [];
@@ -38,7 +38,7 @@
 			if ('value' in element) { return element.value; }
 			return element.innerHTML || '';
 		};
-		
+
 		// SETINPUTVALUE: tries to determine an input type and set an appropriate value
 		service.setInputValue = function(element, value) {
 			if (element.matches('input[type=radio]') || element.matches('input[type=checkbox]')) {
@@ -57,19 +57,19 @@
 			if ('value' in element) { element.value = value; return; }
 			element.innerHTML = service.encode((value || '').toString());
 		};
-		
+
 		// SETSTYLE: Adds a styling rule to the page
 		service.setStyle = function(selector, style) {
 			if (selector in styles) { return; }
 			styles[selector] = style;
-			
+
 			var concat = '';
 			for (var key in styles) {
 				concat = concat + key + ' ' + styles[key] + '\r\n';
 			}
 			styleblock.innerHTML = concat;
 		};
-		
+
 		// REMOVECLASS: removes a class from the element
 		service.removeClass = function(element, cls) {
 			cls = cls.replace(/[^a-z0-9\-_]+/gi, '');
@@ -77,7 +77,7 @@
 			value = value.replace(new RegExp('(\\s|^)' + cls + '(\\s|$)', 'gi'), '');
 			element.setAttribute('class', value.trim());
 		};
-		
+
 		// ADDCLASS: adds a class to the element
 		service.addClass = function(element, cls) {
 			cls = cls.replace(/[^a-z0-9\-_]+/gi, '');
@@ -86,25 +86,25 @@
 			value = value + ' ' + cls;
 			element.setAttribute('class', value.trim());
 		};
-		
+
 		// HASCLASS: determines if a class exists
 		service.hasClass = function(element, cls) {
 			cls = cls.replace(/[^a-z0-9\-_]+/gi, '');
 			var value = element.getAttribute('class') || '';
 			return !!value.match(new RegExp('(\\s|^)' + cls + '(\\s|$)', 'gi'));
 		};
-		
+
 		// TOGGLECLASS: toggles a class on the element
 		service.toggleClass = function(element, cls) {
 			if (service.hasClass(element, cls)) { service.removeClass(element, cls); }
 			else { service.addClass(element, cls); }
 		};
-		
+
 		// INDOM: See utilities
 		service.indom = function(element) {
 			return utils.indom(element);
 		}
-		
+
 		// FORMATPARAMETERS: converts an object to a uri query string
 		service.formatParameters = function(obj) {
 			if (typeof(obj) === 'string') { return obj; }
@@ -115,7 +115,7 @@
 			}
 			return params.join('&');
 		}
-		
+
 		// FORMATURI: concats a uri with a query object or string
 		service.formatUri = function(uri, params) {
 			if (!params) { return uri; }
@@ -125,7 +125,7 @@
 				? uri + '&' + params
 				: uri + '?' + params;
 		}
-		
+
 		// PARSEQUERY: converts a query string into an object
 		service.parseQuery = function(params) {
 			var result = {};
@@ -136,13 +136,13 @@
 				var key = decodeURIComponent(parts[0] || '');
 				var value = decodeURIComponent(parts[1] || '');
 				if (!key || !value) { continue; }
-				if (typeof(result[key]) === 'string') { result[key] = [result[key], value]; }				
+				if (typeof(result[key]) === 'string') { result[key] = [result[key], value]; }
 				else if (key in result) { result[key].push(value); }
 				else { result[key] = value; }
 			}
 			return result;
 		}
-		
+
 		Object.freeze(service);
 	}]);
 })();
