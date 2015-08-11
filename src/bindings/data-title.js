3a41396afa3b512833a0e1d81818dd93bc1de3b9
Mark Greenwald <markerdink@gmail.com>
2015-08-11 14:48:21 -0700

    ...

--- src/bindings/data-title.js
+++ src/bindings/data-title.js
@@ -1,10 +1,11 @@
 // Binds this element's "TITLE" attribute to a value on the ViewModel
-Application.extend.binding('data-title', ['view', 'viewmodel', 'html', function(view, viewmodel, html) {
-   var path = view.element.getAttribute('data-title');
+Curvy.register.binding('data-title', ['viewmodel'], function(viewmodel) {
+   if (!viewmodel) { throw 'data-title: no viewmodel defined'; }
+   var path = this.element.getAttribute('data-title');
    var update = function() {
       var value = viewmodel.path(path) || '';
       view.element.setAttribute('title', value);
    };
    viewmodel.watch(path, update);
    update();
-}]);
+});
